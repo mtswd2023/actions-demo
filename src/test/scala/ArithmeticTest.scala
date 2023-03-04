8f502d9d8bb6cc39efbc6190475d004cf26b42fe
Roman Tkalenko <rmn.tk.ml@gmail.com>
2023-03-04 15:05:15 +0200

    feat: add multiplication logic

--- src/test/scala/ArithmeticTest.scala
+++ src/test/scala/ArithmeticTest.scala
@@ -2,6 +2,8 @@ package com.tkroman.kpi
 
 import munit.FunSuite
 
+import scala.util.Random
+
 class ArithmeticTest extends FunSuite {
   private val arithmetic = new Arithmetic
 
@@ -17,5 +19,19 @@ class ArithmeticTest extends FunSuite {
     val x = 10123123
     assertEquals(arithmetic.sum(0, x), x)
     assertEquals(arithmetic.sum(x, 0), x)
+  }
+
+  test("mul 1") {
+    val x = Random.nextInt()
+    assertEquals(arithmetic.mul(x, 1), x)
+    assertEquals(arithmetic.mul(1, x), x)
+  }
+
+  test("mul specific case") {
+    assertEquals(arithmetic.mul(123, 2), 246)
+  }
+
+  test("mul overflow") {
+    assertEquals(arithmetic.mul(Int.MaxValue, 2), Int.MinValue)
   }
 }
